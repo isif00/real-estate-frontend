af73666cc693a780df78cdef13a61a4cacab149a
YacineMK <merroukiyacine06@gmail.com>
2024-05-03 23:30:02 +0100

    add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Client from './Pages/Client.jsx';
+import RealEstate from './Pages/RealEstate.jsx';
+import Transaction from './Pages/Transaction.jsx';
+
+import { NavBar } from './components/MainComponents/NavBar/NavBar'
+import SideNavBar from './components/MainComponents/SideNavBar/SideNavBar'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Client />,
+  },
+  {
+    path: "/RealEstate",
+    element: <RealEstate />,
+  },
+  {
+    path: "/Transaction",
+    element: <Transaction />,
+  }
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <SideNavBar />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
